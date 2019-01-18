db94daa4ddfbaec2c74c412f3232f413113fbaf8
egorklimov <klim.electronicmail@gmail.com>
2019-01-18 18:21:33 +0300

    HW3: init django project

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@ sudo unlink /etc/nginx/sites-enabled/default
 sudo rm /etc/nginx/nginx.conf
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
